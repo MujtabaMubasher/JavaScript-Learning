137099d621f262f9ea4ddc786f03761dd885dd2c
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-01 08:14:55 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const StudentID = 12346;
+let emailId = "mujtabamubasher11@gmail.com";
+var city = "lahore";
+country = "pakistan"
+
+let mujtaba;
+
+//StudentID=2551 => Not Allowed 
+
+/* Prefer not to use var
+   because of issue in block scope and functional scope
+*/
+
+emailId="kddnkns@dnkdn.com"
+city="islamabad"
+country="India"
+console.log(StudentID);
+console.table([StudentID,emailId,city,country,mujtaba])
