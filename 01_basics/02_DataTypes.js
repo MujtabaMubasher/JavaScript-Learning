b148a872a2428940ae4bb511d2b4b93824c9b9d2
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-03 04:28:06 +0000

    container config done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // Treat all  Js codes  as newer version of Js  
+
+//alert("Hello"); => We are using Node Js, This is use in browser
+
+console.log(2+
+    
+    3)  // code Readability Should be high 
+
+console.log("nkfsnfks") 
+
+let name = "Mujtaba"
+let number= 45
+//let isLogin = ture
+let sate;
+let state = null
+
+/*
+
+Primitive DataTypes:
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standalone Value
+//undefined
+//symbole => unique
+
+*/
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+/*
+
+
+
+*/
