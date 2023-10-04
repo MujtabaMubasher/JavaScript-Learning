ac4e708f745af14e15b6e5001b3c724015c1dbeb
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-04 06:51:14 +0000

    Done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,8 +21,8 @@ console.log(valInNumber);
 
 let isLogedIn = "Mujtaba"
 let booleanIsLogedIn = Boolean(isLogedIn)
-console.log(typeof booleanIsLogedIn);
-console.log(booleanIsLogedIn);
+//console.log(typeof booleanIsLogedIn);
+//console.log(booleanIsLogedIn);
 
 /*
 
@@ -34,9 +34,9 @@ console.log(booleanIsLogedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+//console.log(typeof someNumber);
+//console.log(typeof stringNumber);
+//console.log(stringNumber);
 
 /*
 
@@ -44,3 +44,59 @@ console.log(stringNumber);
 
 */
 
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+//  console.log(+true);
+//  console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++; // postfix
+//console.log(gameCounter);
+++gameCounter; // Prefix
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+/****************************Prefix & Postfix **************************** */
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
