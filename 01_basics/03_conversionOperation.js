7fbdd03a07d177aa11c6fef27a8f4d4c28f08b0e
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-04 06:21:32 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,46 @@
+/*let score ="true"
+console.log(typeof score);
+console.log(typeof(score));
+let valInNumber = Number(score)
+console.log(typeof(valInNumber));
+console.log(valInNumber);
+*/
+
+
+/*
+
+// "33" => 33
+// 33avc => NaN
+// null => 0
+// undefined => NaN
+// true => 1 & false => 0
+//"true" => NaN
+
+
+*/
+
+let isLogedIn = "Mujtaba"
+let booleanIsLogedIn = Boolean(isLogedIn)
+console.log(typeof booleanIsLogedIn);
+console.log(booleanIsLogedIn);
+
+/*
+
+// 1 => true & 0 => false & 100 => true
+//"" => false
+// "Mujtaba" => true
+
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+/*
+
+//
+
+*/
+
