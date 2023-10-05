8299abedc2b96f10ccb0207bccccf269fddf917d
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-05 06:36:36 +0000

    done

---
+++ 01_basics/stack_heapMemory.js
@@ -0,0 +1,30 @@
+// Stack (primitive), Heap (Non-primitive)
+
+
+
+let stack = "mujtaba"
+
+stack1 = stack
+
+
+stack1 = "mubasher"
+
+console.log(stack1);
+console.log(stack);
+
+
+let heap = "irtaza"
+
+
+
+let heap1 ={
+    email: "mujtabamubasher11@gmail.com",
+   // id: 0124
+}
+
+heap2 = heap1
+
+heap2.email = "mujtabamubasher786@gmail.com"
+
+console.log(heap2.email);
+console.log(heap1.email);
