2a162438f5263d13ffbeacf6123d0831df46bd11
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-10-10 07:38:02 +0000

    done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400 
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+//const otherNumber = 123.9554
+const otherNumber = 12.9554
+//const otherNumber = 1235.9554
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//*********************************Maths*************************************/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(4,8,6,9,1,6,5,4,1));
+// console.log(Math.max(4,6,8,9,3,1,25,68,4,6,58,4));
+
+
+console.log(Math.random());
+console.log(Math.random());
+console.log( Math.floor(Math.random()*10) + 1);
+
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min );
